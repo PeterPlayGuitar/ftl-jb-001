6f2b23cd49a6b69a8d4f26d3f9d2de57eb229414
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-08-24 22:10:32 +0400

    L006 - Static/Instance methods example

--- src/com/company/animals/Dog.java
+++ src/com/company/animals/Dog.java
@@ -1,5 +1,33 @@
 package com.company.animals;
 
 public class Dog extends Animal {
+    public Dog(String name, Integer weight) {
+        super(name, weight);
+    }
 
+    public Dog() {
+    }
+
+    /**
+     * Идём до палки. Чтобы дойти расщитываем разницу и прибывляем к тек. положению пса
+     * @param stickPosition - позиция палки
+     * */
+    public void goToStick(Integer stickPosition){
+        Integer delta = stickPosition - currentPosition;
+        this.runForward(delta);
+    }
+
+    public void whereTheDog(){
+        System.out.println(currentPosition);
+    }
+
+    public static Dog of(String name, Integer weight){
+        return new Dog(name, weight);
+    }
+
+    public static Dog ofHomeless(Integer weight){
+        Dog dog = new Dog();
+        dog.weight = weight;
+        return dog;
+    }
 }
