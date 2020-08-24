314109ee93d394d3d0f0123cf5bdece3b39a94eb
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-08-24 21:51:39 +0400

    L005 - methods example

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 package com.company;
 
-import com.company.L001.HelloWorld;
-import com.company.L002.POJO;
-import com.company.L003.Cat;
+import com.company.animals.Cat;
 
 public class Main {
 
     public static void main(String[] args) {
-        HelloWorld.run();
-        POJO.run();
+       //HelloWorld.run();
+       //POJO.run();
 
 
-        Cat catVar = new Cat();
-        catVar.voice();
-        Cat barsick = new Cat();
+       //Cat catVar = new Cat();
+       //catVar.voice();
+       //Cat barsick = new Cat();
 
-        com.company.L004.Cat catL004 = new com.company.L004.Cat();
-        catL004.voice();
+       //com.company.L004.Cat catL004 = new com.company.L004.Cat();
+       //catL004.voice();
+
+       Cat cat = new Cat("Barsick", 20);
+       System.out.println(cat.jumpHeight());
     }
 }
---
+++ src/com/company/animals/Animal.java
@@ -0,0 +1,20 @@
+package com.company.animals;
+
+public class Animal {
+
+    protected String name;
+    protected Integer weight;
+
+    public Animal(String name, Integer weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Animal(){
+
+    }
+
+    void voice(){
+        System.out.println("Voice!!!");
+    }
+}
