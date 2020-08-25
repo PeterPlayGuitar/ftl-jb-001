1a58ce67c8ea69e7ea6b8de2e78ea6d1584022eb
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-08-25 16:02:18 +0400

    L014 - can fly example

--- src/com/company/animals/Bird.java
+++ src/com/company/animals/Bird.java
@@ -1,17 +1,23 @@
 package com.company.animals;
 
+import java.text.BreakIterator;
+
 public class Bird extends Animal {
     protected Boolean flyModeActive = false;
+
+    public Bird() {
+        canFly = true;
+    }
 
     public void takeOff() {
         flyModeActive = true;
     }
 
-    public void landing(){
+    public void landing() {
         flyModeActive = false;
     }
 
-    public void isTheBirdFlying(){
+    public void isTheBirdFlying() {
         System.out.println(flyModeActive ? "Duck is flying" : "Duck is not flying");
     }
 }
