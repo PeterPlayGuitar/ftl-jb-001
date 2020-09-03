4d57606928efa28ce13115526c3b69732a941083
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-09-03 20:29:42 +0400

    task002 - 5 4 3 2 1 output

--- src/com/company/task002/Task002.java
+++ src/com/company/task002/Task002.java
@@ -2,6 +2,6 @@ package com.company.task002;
 
 public class Task002 {
     public static void run() {
-        System.out.println(String.join(" ", new String[]{"5", "4", "3", "2", "1"}));
+        System.out.println(String.join(" ", new String[]{"5", "4", "3", "2", "1"})); 
     }
 }
