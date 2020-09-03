89fc12d60b25076df975f8641d39b6719166e75a
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-09-03 20:34:11 +0400

    task003 - multiply by 3 table output

---
+++ src/com/company/task003/Task003.java
@@ -0,0 +1,10 @@
+package com.company.task003;
+
+public class Task003 {
+    public static void run() {
+        int multiplyier = 3;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(multiplyier + "*" + i + "=" + (i * multiplyier));
+        }
+    }
+}
